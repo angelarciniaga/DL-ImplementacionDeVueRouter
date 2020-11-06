02a72dc0094638245a128b7c8d7218298389329a
Your Name <you@example.com>
2020-11-06 00:13:42 -0300

    primer commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import bootstrap from '../public/assets/bootstrap/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  bootstrap,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Inicio from '../views/Inicio.vue'
+import SobreMi from '../views/SobreMi.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Inicio',
+    component: Inicio
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/SobreMi',
+    name: 'SobreMi',
+    component: SobreMi
+  },
+  {
+    path: '/Contacto',
+    name: 'Contacto',
+    component: () => import( '../views/Contacto.vue')
+  },
+  {
+    path: '/UltimoPost',
+    name: 'UltimoPost',
+    component: () => import( '../views/UltimoPost.vue')
+  },
+  {
+    path: '/*',
+    name: 'NotFound',
+    component: () => import( '../views/NotFound.vue')
   }
 ]
 
